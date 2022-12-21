1fad355b13db12f4174cd611b605962a4c91ae96
Quang Minh <71371833+kou1153@users.noreply.github.com>
2022-12-21 21:22:23 +0700

    Fix

--- src/Headers/Network/topic.h
+++ src/Headers/Network/topic.h
@@ -1,23 +1,8 @@
 #pragma once
 
-String setupString(String options);
-
-const String deviceName = "espInAC";
-const String roomName = "room1";
-const String packetACK = "ACK";
-const String serverName = "server";
 // Setup topic
 // From device
-const char *topicACDevice = setupString("device").c_str();
-const char *topicACACK = setupString("ack").c_str();
+const char *topicACDevice = "espInACwithLight/room1";
+const char *topicACACK = "espInACwithLight/room1/ACK";
 // To device
-const char *topicACServer = setupString("server").c_str();
-
-String setupString(String options)
-{
-    String temp = deviceName + "/" + roomName;
-    if (options == "device") return temp;
-    else if (options == "ack") return temp += "/" + packetACK;
-    else if (options == "server") return temp += "/" + serverName;
-    else return options;
-}
+const char *topicACServer = "espInACwithLight/room1/server";
